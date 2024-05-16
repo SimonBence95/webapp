162a366c2171f09679c18b9520182a47d48508dd
Simon Bence <112952826+SimonBence95@users.noreply.github.com>
2024-05-16 18:58:11 +0200

    Delete src directory

--- src/theme/components/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './button';
-export * from './form-label';
-export { inputTheme } from './input';
-export * from './menu';
-export * from './modal';
-export * from './number-input';
-export * from './textarea';
